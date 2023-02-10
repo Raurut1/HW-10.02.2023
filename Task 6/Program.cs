c2d605e9c5191b5228fdd002b69ef2dda4e829c3
Raurut <makey@yandex.ru>
2023-02-11 02:49:32 +0300

    Task 6 complete

---
+++ Task 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ведите число");
+int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 1)
+{
+    Console.WriteLine("Число " + a + " Нечётное");
+}
+else
+{
+    Console.WriteLine("Число " + a + " Чётное");
+}
